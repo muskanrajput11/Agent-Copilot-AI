82bd6a10cc0084407165ca9b5cc9ef2813d14d01
muskanrajput11 <muskanrajput1104@gmail.com>
2025-11-02 20:53:41 +0530

    Update CORS middleware to allow all origins

--- app.py
+++ app.py
@@ -26,10 +26,7 @@ app = FastAPI(
 # --- Step 3: Enable CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "https://agent-copilot-ai-sigma.vercel.app", 
-        "https://agent-copilot-ai.vercel.app"  
+    allow_origins=["*"
     ],
     allow_credentials=True,
     allow_methods=["*"], 
